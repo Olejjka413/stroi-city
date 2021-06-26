4bfcb3e11bdaa196fcdfa6362e4b08bab4b219bc
Mari525 <56230146+Mari525@users.noreply.github.com>
2021-06-26 17:50:06 +0300

    слайдер готов

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
         $('.nav').toggleClass('open-menu');
         $('body').toggleClass('fixed-page');
 	});
+        $('#carousel-1').owlCarousel({
+                items: 1,
+                loop: true,
+                dots: true,
+                autoplay: true,
+                smartSpeed: 600
+        });
 });
